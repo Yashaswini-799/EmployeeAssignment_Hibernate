6542e8c61077011bccf08bed8d9e9e745f19f2d4
Yashaswini-799 <79266570+Yashaswini-799@users.noreply.github.com>
2022-03-04 15:02:31 +0530

    Add files via upload

---
+++ empassignment/empassignment/src/main/java/com/assignment/emp/empassignment/EmployeeApplication.java
@@ -0,0 +1,36 @@
+package com.assignment.emp.empassignment;
+
+import org.slf4j.Logger;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.assignment.emp.empassignment.entity.Employee;
+import com.assignment.emp.empassignment.repository.EmployeeRepository;
+
+
+@SpringBootApplication
+public class EmployeeApplication implements CommandLineRunner {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	private EmployeeRepository  repository;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeApplication.class, args);
+	}
+
+
+	@Override
+	public void run(String... args) throws Exception {
+		Employee employee = repository.findById(10005L);
+		logger.info("employee 10005 = {}",employee);
+	repository.deleteById(10001L);
+		repository.save(new Employee("ram","pothineni"));
+	}
+
+}
---
+++ empassignment/empassignment/target/classes/data.sql
@@ -0,0 +1,30 @@
+insert into employee(id, name, last_name) 
+values(10001,'Yashu','polamarasetti');
+insert into employee (id, name, last_name) 
+values(10002,'Gayathri' ,'Polam' );
+insert into employee(id, name, last_name ) 
+values(10003,'Mahesh', 'Polam');
+insert into employee(id, name, last_name ) 
+values(10004,'Sanjeev', 'Shetty');
+insert into employee(id, name, last_name ) 
+values(10005,'Akhila', 'Shetty');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
